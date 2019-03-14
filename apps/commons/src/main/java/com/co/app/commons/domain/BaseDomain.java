bbb5ff324c4429769f886100674a4c90c336cd53
Luis Colmenarez <luis.miguelcv2@gmail.com>
2019-03-14 15:37:55 -0400

    JpaAuditing configuration (#5)

--- apps/commons/src/main/java/com/co/app/commons/domain/BaseDomain.java
+++ apps/commons/src/main/java/com/co/app/commons/domain/BaseDomain.java
@@ -6,9 +6,12 @@ package com.co.app.commons.domain;
 import java.time.LocalDateTime;
 
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 
@@ -16,6 +19,8 @@ import org.springframework.format.annotation.DateTimeFormat.ISO;
  * @author alobaton
  *
  */
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseDomain extends BaseEntity {
 
 	@CreatedDate
