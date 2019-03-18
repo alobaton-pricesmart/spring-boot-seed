573fb70c2c8e7b7f6c41e2f783988d69f0eb0e89
Luis Colmenarez <luis.miguelcv2@gmail.com>
2019-03-18 14:54:37 -0400

    Add Connection LDAP

--- apps/auth/src/main/java/com/co/app/auth/configuration/WebSecurityConfiguration.java
+++ apps/auth/src/main/java/com/co/app/auth/configuration/WebSecurityConfiguration.java
@@ -4,6 +4,7 @@
 package com.co.app.auth.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,6 +16,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.co.app.auth.services.user.impl.CustomUserDetailsService;
+import org.apache.commons.logging.LogFactory;
+import org.apache.commons.logging.Log;
 
 /**
  * @author alobaton
@@ -24,6 +27,26 @@ import com.co.app.auth.services.user.impl.CustomUserDetailsService;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	private static final Log LOGGER = LogFactory.getLog(WebSecurityConfiguration.class);
+
+	@Value("${ldap.urls}")
+	private String ldapUrls;
+
+	@Value("${ldap.base.dn}")
+	private String ldapBaseDn;
+
+	@Value("${ldap.username}")
+	private String ldapSecurityPrincipal;
+
+	@Value("${ldap.password}")
+	private String ldapPrincipalPassword;
+
+	@Value("${ldap.user.dn.pattern}")
+	private String ldapUserDnPattern;
+
+	@Value("${ldap.enabled}")
+	private String ldapEnabled;
 
 	@Autowired
 	private CustomUserDetailsService customUserDetailsService;
@@ -67,7 +90,14 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	 */
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+		if (Boolean.parseBoolean(this.ldapEnabled)) {
+			LOGGER.info("Autenticando contra LADP");
+			auth.ldapAuthentication().contextSource().url(this.ldapUrls + this.ldapBaseDn)
+					.managerDn(ldapSecurityPrincipal).managerPassword(this.ldapPrincipalPassword).and()
+					.userDnPatterns(this.ldapUserDnPattern);
+		} else {
+			LOGGER.info("Autenticando contra DB");
+			auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+		}
 	}
-
 }
