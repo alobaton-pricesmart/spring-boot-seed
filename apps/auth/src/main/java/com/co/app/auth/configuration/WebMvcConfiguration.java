7f528de5aa511a1cc45a6676238ab2722a6656f9
alobaton <alobaton.restrepo@gmail.com>
2019-03-28 15:44:18 -0500

    Fix swagger view

--- apps/auth/src/main/java/com/co/app/auth/configuration/WebMvcConfiguration.java
+++ apps/auth/src/main/java/com/co/app/auth/configuration/WebMvcConfiguration.java
@@ -57,6 +57,7 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 	 */
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
 		registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
 		registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
 		registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
