c322fc6dae4fc7206ee4d5b5c6ff92511b800617
alobaton <alobaton.restrepo@gmail.com>
2019-09-22 10:55:41 -0500

    Delete masters module. It's a bad practice have a single master entity. The idea is to guarantee a low coupling.

--- apps/core/src/main/java/com/co/app/core/configuration/CoreConfiguration.java
+++ apps/core/src/main/java/com/co/app/core/configuration/CoreConfiguration.java
@@ -6,10 +6,8 @@ package com.co.app.core.configuration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import com.co.app.masters.configuration.MastersConfiguration;
 import com.co.app.settings.configuration.SettingsConfiguration;
 
 /**
@@ -20,9 +18,7 @@ import com.co.app.settings.configuration.SettingsConfiguration;
 @EnableAutoConfiguration
 @EnableJpaAuditing
 // Import your property sources here...
-@PropertySource("classpath:/auth/auth.properties")
-@PropertySource("classpath:/email/email.properties")
 //Import your app modules here...
-@Import({ MastersConfiguration.class, SettingsConfiguration.class })
+@Import({ SettingsConfiguration.class })
 public class CoreConfiguration {
 }
